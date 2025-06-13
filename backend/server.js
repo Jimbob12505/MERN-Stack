daaa43364b2e9318f492bbd82d05c9b7c5ea0388
Vatsal Labh <vatsallabh@Vatsals-MacBook-Air-2.local>
2025-06-12 23:26:38 -0400

    Developed 4 major end points in server.py. Changed dev script to use nodemon and added start script

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,33 @@
 const express = require("express")
 
-const app = express()
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+  // recieve notes
+  res.status(200).send("Received notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  // create note
+  res.status(201).json({
+    "message": "post created successfully"
+  });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  // update note
+  res.status(200).json({
+    "message": "note updated successfully"
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  // update note
+  res.status(200).json({
+    "message": "note deleted successfully"
+  });
+});
 
 app.listen(5001, () => {
-  console.log("Server started on port 5001")
-})
+  console.log("Server started on port 5001");
+}); 
