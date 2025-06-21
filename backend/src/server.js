01870f743d474bef9c3fcdc02f2470e3e23e43eb
Vatsal Labh <vatsallabh@Vatsals-MacBook-Air-2.local>
2025-06-21 19:02:10 -0400

    Created middleware for rate limiting requests using upstash and redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import notesRoutes from "./routes/notesRoutes.js";
 import express from "express"
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,8 +12,17 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json()); // middleware
-app.use("/api/notes", notesRoutes);
+app.use(rateLimiter);
+app.use((req,res,next) => {
+  console.log("NEW REQUEST RECIEVED");
+  console.log("________________\n");
+  console.log(`Method: ${req.method}\nEndpoint: ${req.url}`);
+  console.log(req.body);
+  console.log("________________\n");
+  next();
+});
 
+app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started on port:", PORT);
