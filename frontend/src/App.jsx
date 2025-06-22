b2db199b089fa80e2f4a6efd369a5eafc401c253
Vatsal Labh <vatsallabh@Vatsals-MacBook-Air-2.local>
2025-06-22 19:05:12 -0400

    Added tailwindcss and daisyUI integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <button className="btn btn-outline btn-info"> Click me </button>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>} />
